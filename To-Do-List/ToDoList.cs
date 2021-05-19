2328fec4f5ac182596f9b670cdca602a74a91ee2
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-05-19 02:30:16 -0300

    Definir as propriedades da lista

---
+++ To-Do-List/ToDoList.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace To_Do_List
+{
+    public class ToDoList
+    {
+        public DateTime Date { get; set; }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool Execution { get; set; }
+    }
+}
