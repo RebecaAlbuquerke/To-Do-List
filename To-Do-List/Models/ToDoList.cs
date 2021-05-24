76a7c52214a87522ad425340664814be1c8046fa
Rebeca Albuquerque <rebecaalbuquerke@gmail.com>
2021-05-24 14:35:46 -0300

    Relacionamento entre a entidade ToDoItem com ToDoList

---
+++ To-Do-List/Models/ToDoList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace To_Do_List.Models
+{
+    public class ToDoList
+    {
+        public ToDoItem Item { get; set; }
+
+        public string Theme { get; set; }
+    }
+}
